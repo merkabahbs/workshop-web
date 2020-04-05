bb1f40e38f53af41a3e2aa701826d73a665e2ef8
Antonio Barandica <anjose.121@gmail.com>
2020-04-05 18:57:09 -0500

    Add footer section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,109 +1,53 @@
 import React from 'react'
-import { Link } from 'gatsby'
 
-import logo from '../img/logo.svg'
 import facebook from '../img/social/facebook.svg'
 import instagram from '../img/social/instagram.svg'
-import twitter from '../img/social/twitter.svg'
-import vimeo from '../img/social/vimeo.svg'
+import linkedin from '../img/social/linkedin.svg'
 
 const Footer = class extends React.Component {
   render() {
     return (
-      <footer className="footer has-background-black has-text-white-ter">
-        <div className="content has-text-centered">
-          <img
-            src={logo}
-            alt="Kaldi"
-            style={{ width: '14em', height: '10em' }}
-          />
-        </div>
-        <div className="content has-text-centered has-background-black has-text-white-ter">
-          <div className="container has-background-black has-text-white-ter">
-            <div className="columns">
-              <div className="column is-4">
-                <section className="menu">
-                  <ul className="menu-list">
-                    <li>
-                      <Link to="/" className="navbar-item">
-                        Home
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/about">
-                        About
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/products">
-                        Products
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/contact/examples">
-                        Form Examples
-                      </Link>
-                    </li>
-                    <li>
-                      <a
-                        className="navbar-item"
-                        href="/admin/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        Admin
-                      </a>
-                    </li>
-                  </ul>
-                </section>
-              </div>
-              <div className="column is-4">
-                <section>
-                  <ul className="menu-list">
-                    <li>
-                      <Link className="navbar-item" to="/blog">
-                        Latest Stories
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/contact">
-                        Contact
-                      </Link>
-                    </li>
-                  </ul>
-                </section>
-              </div>
-              <div className="column is-4 social">
-                <a title="facebook" href="https://facebook.com">
-                  <img
-                    src={facebook}
-                    alt="Facebook"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="twitter" href="https://twitter.com">
-                  <img
-                    className="fas fa-lg"
-                    src={twitter}
-                    alt="Twitter"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="instagram" href="https://instagram.com">
-                  <img
-                    src={instagram}
-                    alt="Instagram"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="vimeo" href="https://vimeo.com">
-                  <img
-                    src={vimeo}
-                    alt="Vimeo"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-              </div>
+      <footer className="footer has-background-white has-text-link">
+        <div className="container">
+          <div className="columns">
+            <div className="column">
+              <h1 className="title has-text-link">
+                Encontremos lo que te hará grande
+              </h1>
+            </div>
+            <div className="column is-hidden-mobile" />
+          </div>
+          <div className="columns">
+            <div className="column is-flex">
+              <a
+                title="facebook"
+                target="_blank"
+                href="https://www.facebook.com/Merkabahbs"
+              >
+                <img src={facebook} alt="Facebook" />
+              </a>
+              <a
+                title="instagram"
+                target="_blank"
+                href="https://www.instagram.com/merkabahbs/"
+              >
+                <img src={instagram} alt="Instagram" />
+              </a>
+              <a
+                title="linkedin"
+                target="_blank"
+                href="https://www.linkedin.com/company/merkabah-business-solutions/"
+              >
+                <img src={linkedin} alt="Linkedin" />
+              </a>
+              <h4 className="subtitle is-inline is-hidden-mobile">
+                <a href="https://merkabahbs.com">www.merkabahbs.com</a>
+              </h4>
+            </div>
+            <div className="column is-hidden-desktop">
+              <h4 className="subtitle">
+                <a href="https://merkabahbs.com">www.merkabahbs.com</a>
+              </h4>
             </div>
           </div>
         </div>
